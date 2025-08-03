00563e0eb2be4fb17665f701d7474b01245857e0
NeroQue <dominikbratek11@gmail.com>
2025-08-03 17:29:15 +0200

    Refactor chirp validation handler

--- ValidateChirp.go
+++ ValidateChirp.go
@@ -3,7 +3,42 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
+
+var respBody = map[string]interface{}{}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respBody["error"] = msg
+	data, _ := json.Marshal(respBody)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func profaneReplace(s string) string {
+	profaneWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	words := strings.Split(s, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := profaneWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
+}
 
 func HandlerValidateChirps(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
@@ -12,28 +47,17 @@ func HandlerValidateChirps(w http.ResponseWriter, r *http.Request) {
 
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
-	respBody := map[string]interface{}{}
+
 	err := decoder.Decode(&params)
 	if err != nil {
-		respBody["error"] = "Something went wrong"
-		data, _ := json.Marshal(respBody)
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write(data)
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 	if len(params.Body) > 140 {
-		respBody["error"] = "Chirp is too long"
-		data, _ := json.Marshal(respBody)
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write(data)
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
 		return
 	}
-	respBody["valid"] = true
-	data, _ := json.Marshal(respBody)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(data)
+	respBody["cleaned_body"] = profaneReplace(params.Body)
+	respondWithJSON(w, http.StatusOK, respBody)
 	return
 }
